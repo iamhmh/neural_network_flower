2f6dd9c409fe0e970ded7d6d347cdd9b963bd512
iamhmh <hichem.gouia@epitech.eu>
2024-11-16 15:40:21 +0100

    [add] tracking performance w/ loss evolution graph

--- neural_network_flower.py
+++ neural_network_flower.py
@@ -1,4 +1,5 @@
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
 
 x_entry =  np.array(([3, 1.5], [2, 1], [4, 1.5], [3, 1], [3.5,0.5], [2,0.5], [5.5,1], [1,1], [4,1.5]), dtype=float)
 y = np.array(([1], [0], [1],[0],[1],[0],[1],[0]), dtype=float) # 1 = red, 0 = blue
@@ -18,6 +19,8 @@ class Neural_Network(object):
         # weights
         self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # matrice 2x3 entry neural to hidden neural
         self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # matrice 3x1 hidden neural to ouput neural
+
+        self.loss_history = [] # loss history for the training data
 
         # propagation function
     def forward(self, X):
@@ -50,6 +53,8 @@ class Neural_Network(object):
     def train(self, X, y):
         o = self.forward(X)
         self.backward(X, y, o)
+        loss = np.mean(np.square(y - o))
+        self.loss_history.append(loss)
 
     # prediction function
     def prediction(self):
@@ -74,3 +79,10 @@ for i in range(1000):
     NN.train(X,y)
 
 NN.prediction()
+
+plt.plot(NN.loss_history)
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Loss evolution during training')
+plt.savefig('charts/loss_evolution.png')
+plt.show()
