4d1e376f10f2db503fb3248b24e2340c4292ef86
iamhmh <hichem.gouia@epitech.eu>
2024-11-16 16:00:58 +0100

    [add] drawing the neural network structure w/ plt

--- neural_network_flower.py
+++ neural_network_flower.py
@@ -1,5 +1,9 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+if not os.path.exists('images'):
+    os.makedirs('images')
 
 x_entry =  np.array(([3, 1.5], [2, 1], [4, 1.5], [3, 1], [3.5,0.5], [2,0.5], [5.5,1], [1,1], [4,1.5]), dtype=float)
 y = np.array(([1], [0], [1],[0],[1],[0],[1],[0]), dtype=float) # 1 = red, 0 = blue
@@ -58,7 +62,6 @@ class Neural_Network(object):
 
     # prediction function
     def prediction(self):
-
         print("Predicted Data after train : ")
         print("Entry : \n" + str(xPredicted))
         print("Ouput : \n" + str(self.forward(xPredicted)))
@@ -67,6 +70,51 @@ class Neural_Network(object):
             print("The flower is blue ! \n")
         else:
             print("The flower is red ! \n")
+    
+def draw_neural_network(layer_sizes):
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.axis('off')
+    n_layers = len(layer_sizes)
+    v_spacing = (1.0 - 0.2) / float(max(layer_sizes))
+    h_spacing = 1.0 / float(n_layers - 1) if n_layers > 1 else 1.0
+
+    layer_colors = ['skyblue', 'lightgreen', 'salmon', 'plum', 'khaki']
+    
+    for i, layer_size in enumerate(layer_sizes):
+        layer_top = (1.0 - (layer_size - 1) * v_spacing) / 2.0
+        color = layer_colors[i % len(layer_colors)]
+        for j in range(layer_size):
+            circle = plt.Circle((i * h_spacing + 0.1, layer_top + j * v_spacing), v_spacing / 4,
+                                color=color, ec='black', zorder=4)
+            ax.add_artist(circle)
+            ax.text(i * h_spacing + 0.1, layer_top + j * v_spacing, f'N{j+1}',
+                    fontsize=9, ha='center', va='center')
+    
+    for i, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
+        layer_top_a = (1.0 - (layer_size_a - 1) * v_spacing) / 2.0
+        layer_top_b = (1.0 - (layer_size_b - 1) * v_spacing) / 2.0
+        for j in range(layer_size_a):
+            for k in range(layer_size_b):
+                line = plt.Line2D([i * h_spacing + 0.1, (i + 1) * h_spacing + 0.1],
+                                  [layer_top_a + j * v_spacing, layer_top_b + k * v_spacing],
+                                  c='gray', lw=1)
+                ax.add_artist(line)
+    
+    for i in range(n_layers):
+        layer_label = ''
+        if i == 0:
+            layer_label = 'Input'
+        elif i == n_layers - 1:
+            layer_label = 'Output'
+        else:
+            layer_label = f'Hidden {i}'
+        ax.text(i * h_spacing + 0.1, 1.05, layer_label, fontsize=12, ha='center', va='bottom')
+    
+    ax.set_xlim(0, 1.0 + 0.2)
+    ax.set_ylim(0, 1.0 + 0.2)
+    plt.tight_layout()
+    plt.savefig('images/neural_network_structure.png', dpi=300)
+    plt.show()
 
 NN = Neural_Network()
 
@@ -84,5 +132,8 @@ plt.plot(NN.loss_history)
 plt.xlabel('Iteration')
 plt.ylabel('Loss')
 plt.title('Loss evolution during training')
-plt.savefig('charts/loss_evolution.png')
+plt.savefig('images/loss_evolution.png')
 plt.show()
+
+layer_sizes = [NN.inputSize, NN.hiddenSize, NN.outputSize]
+draw_neural_network(layer_sizes)
